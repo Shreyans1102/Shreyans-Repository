fc7fdc08033833aab3d5d5718cf56d11026b5d1a
shreyans jain <shreyans jain@DESKTOP-VQNR776>
2022-03-07 14:45:09 +0530

    Firstclass file will add

---
+++ Selenium/src/firstclass.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class firstclass {
+
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chrome.driver", "./SOFTWARE/chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.youtube.com/");
+
+	}
+
+}
